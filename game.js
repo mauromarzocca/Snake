0110c0a5c882af16278ca60668d07f886ae4b9c2
Mauro Marzocca <57107500+mauromarzocca@users.noreply.github.com>
2022-10-07 17:56:16 +0200

    Update js

---
+++ game.js
@@ -0,0 +1,38 @@
+let board;
+let context;
+let blockSize = 20;
+let cols = 30;
+let rows = 20;
+
+let appleAudio;
+let gameOverAudio;
+
+let snakeX = 0;
+let snakeY = 0;
+let tail = [];
+
+let foodX = 0;
+let foodY = 0;
+
+let score = 0;
+
+let velocityX = 1;
+let velocityY = 0;
+
+let gameOver = false;
+
+window.onload = () => {
+    board = 
+}
+
+function update() {}
+
+function foodPlace() {}
+
+function changeDirection() {}
+
+function gameOverEvent() {}
+
+function createRect() {}
+
+function createText() {}
